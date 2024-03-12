b87a8623de2dec7f56c7b367e6c7ae5ba5b1b086
LiZhang-Canada <zhl924505@gmail.com>
2024-03-12 14:19:11 -0600

    finish react router, add one test page to try the router, will delete later

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WarehousePage from './pages/WarehousePage/WarehousePage';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="App">
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+
+          <Route path="/" element={<WarehousePage />}/>
+          <Route path="/warehouses" element={<WarehousePage />}/>
+          {/* <Route path="/warehouses/:id" element={<WarehouseDetailPage />}/>
+          <Route path="/warehouses/:id/edit" element={<WarehouseEdit />}/>
+          <Route path="/warehouses/add" element={<WareHouseAdd />}/>
+
+          <Route path="/inventory" element={<InventoryPage />}/>
+          <Route path="/inventory/:id" element={<InventoryDetailPage />}/>
+          <Route path="/inventory/:id/edit" element={<InventoryEdit />}/>
+          <Route path="/inventory/add" element={<InventoryAdd />}/> */}
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -0,0 +1,8 @@
+function WarehousePage(){
+    return (
+        <>
+            <p>Hello World!</p>
+        </>
+    )
+}
+export default WarehousePage;
