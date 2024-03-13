2e23b124a38b84c2827fac2af608d85bdbd65094
LiZhang-Canada <zhl924505@gmail.com>
2024-03-12 19:13:49 -0600

    finish the styling for delete modal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,22 +1,24 @@
 import './DeleteModal.scss';
+import close from '../../assets/icons/close-24px.svg';
 
 function DeleteModal({ name, isOpen, onCancel, onConfirm }) {
   if (!isOpen) return null; // 如果isOpen为false，不渲染模态
   return (
     <div className="deletemodal">
+      <img src={close} alt="close" className="deletemodal__close" onClick={onCancel}/>
       <h1>Delete {name} warehouse?</h1>
-      <p>
+      <p className="p1 deletemodal__text">
         Please confirm that you’d like to delete the {name} from the list of
         warehouses. You won’t be able to undo this action.
       </p>
-      <button
+      <button className="secondary-button deletemodal__cancel"
         onClick={() => {
           onCancel(); // 关闭模态
         }}
       >
         Cancel
       </button>
-      <button
+      <button className="delete-button deletemodal__delete"
         onClick={() => {
           onConfirm(); // 处理确认动作，比如导航
         }}
