4a2f289d3941f81bbb40cf9ce75d028658c34ca7
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-13 16:06:28 -0600

    adds axios request

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,8 +1,31 @@
-function InventoryPage(){
-    return (
-        <>
-            <p>Hello InventoryPage!</p>
-        </>
-    )
+import "./inventoryPage.scss";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function InventoryPage() {
+  const [inventoryList, setInventoryList] = useState([]);
+  const baseUrl = "http://localhost:8080";
+
+  useEffect(() => {
+    const fetchInventory = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/api/inventory`);
+        document.title = "Inventory | InStock";
+        setInventoryList(response.data);
+      } catch (error) {
+        console.error(`error:  ${error}`);
+      }
+    };
+    fetchInventory();
+  }, []);
+
+  console.log(inventoryList);
+
+  return (
+    <>
+      <p>Hello InventoryPage!</p>
+    </>
+  );
 }
 export default InventoryPage;
