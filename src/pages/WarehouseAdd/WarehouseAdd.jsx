f57716de47bad9700f473be826176fae026440f8
MoeyR <ranm@sheridancollege.ca>
2024-03-14 16:44:13 -0400

    added title-section and styles for WarehouseAdd Page

--- src/pages/WarehouseAdd/WarehouseAdd.jsx
+++ src/pages/WarehouseAdd/WarehouseAdd.jsx
@@ -1,8 +1,33 @@
+import './WarehouseAdd.scss';
+import { Link } from 'react-router-dom';
+import backIcon from '../../assets/icons/arrow_back-24px.svg';
+
 function WarehouseAdd(){
     return (
-        <>
-            <p>Hello WarehouseAdd!</p>
-        </>
+        <section className='main'>
+            {/* ----- title section ----- */}
+            <section className='title-section'>
+                <Link to={'/'} className='title-section__back-link'>
+                    <img className='title-section__back-icon' src={backIcon} alt="back-button" />
+                </Link>
+                <h1 className='title-section__page-title'>Add New Warehouse</h1>
+            </section>
+            {/* ----- form ----- */}
+            <form>
+                <section>
+                    <section>
+
+                    </section>
+                    <section>
+                        
+                    </section>
+                </section>
+                <section className='buttons'>
+
+                </section>
+            </form>
+            
+        </section>
     )
 }
 export default WarehouseAdd;
