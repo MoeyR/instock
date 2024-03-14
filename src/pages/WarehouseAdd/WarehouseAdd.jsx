1a367baf7f5d863188ca3e87cb676039ef597daf
MoeyR <ranm@sheridancollege.ca>
2024-03-14 19:48:08 -0400

    added styles for the form in WarehouseAdd Page

--- src/pages/WarehouseAdd/WarehouseAdd.jsx
+++ src/pages/WarehouseAdd/WarehouseAdd.jsx
@@ -13,17 +13,52 @@ function WarehouseAdd(){
                 <h1 className='title-section__page-title'>Add New Warehouse</h1>
             </section>
             {/* ----- form ----- */}
-            <form>
-                <section>
-                    <section>
-
+            <form className='form'>
+                <section className='form-wrap'>
+                    {/* warehouse details */}
+                    <section className='form__label-inputs-wrap'>
+                        <h2 className='subtitle'>Warehouse Details</h2>
+                        <label className='form__labels' >
+                            <h3 className="label-text">Warehouse Name</h3>
+                            <input className='form-input' type="text" id='warehouseName' placeholder='Warehouse Name'/>
+                        </label>
+                        <label className='form__labels' htmlFor="address">
+                            <h3 className="label-text">Street Address</h3>
+                            <input className='form-input' type="text" id='address' placeholder='Street Address'/>
+                        </label>
+                        <label className='form__labels' htmlFor="city">
+                            <h3 className="label-text">City</h3>
+                            <input className='form-input' type="text" id='city' placeholder='City'/>
+                        </label>
+                        <label className='form__labels' htmlFor="country">
+                            <h3 className="label-text">Country</h3>
+                            <input className='form-input' type="text" id='country' placeholder='Country'/>
+                        </label>
                     </section>
-                    <section>
-                        
+                    {/* contact details */}
+                    <section className='form__label-inputs-wrap'>
+                        <h2 className='subtitle'>Contact Details</h2>
+                        <label className='form__labels' htmlFor="contactName">
+                            <h3 className="label-text">Contact Name</h3>
+                            <input className='form-input' type="text" id='contactName' placeholder='Contact Name'/>
+                        </label>
+                        <label className='form__labels' htmlFor="position">
+                            <h3 className="label-text">Position</h3>
+                            <input className='form-input' type="text" id='position' placeholder='Position'/>
+                        </label>
+                        <label className='form__labels' htmlFor="phoneNumber">
+                            <h3 className="label-text">Phone Number</h3>
+                            <input className='form-input' type="text" id='phoneNumber' placeholder='Phone Number'/>
+                        </label>
+                        <label className='form__labels' htmlFor="email">
+                            <h3 className="label-text">Email</h3>
+                            <input className='form-input' type="text" id='email' placeholder='Email'/>
+                        </label>
                     </section>
                 </section>
-                <section className='buttons'>
-
+                <section className='form__buttons'>
+                    <button className='secondary-button'>Cancel</button>
+                    <button className='primary-button'>+ Add Warehouse</button>
                 </section>
             </form>
             
