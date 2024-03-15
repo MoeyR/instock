a893d6b727eee4b160c72b0e0b06d43cf4dc790b
LiZhang-Canada <zhl924505@gmail.com>
2024-03-14 20:19:40 -0600

    except warehouses list page refresh after delete, others work, no styling

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,31 +1,46 @@
-import './DeleteModal.scss';
-import close from '../../assets/icons/close-24px.svg';
+import "./DeleteModal.scss";
+import close from "../../assets/icons/close-24px.svg";
 
-function DeleteModal({ name, isOpen, onCancel, onConfirm }) {
+function DeleteModal({ name, id, isOpen, onCancel, onConfirm }) {
   if (!isOpen) return null; // 如果isOpen为false，不渲染模态
+  const handleDelete = async (event) => {
+    try {
+      await axios.delete(`http://localhost:8080/api/warehouses/${id}`);
+      onConfirm();
+    } catch (error) {
+      console.log(`error is ${error}`);
+    }
+  };
+
   return (
-    <div className="deletemodal">
-      <img src={close} alt="close" className="deletemodal__close" onClick={onCancel}/>
-      <h1>Delete {name} warehouse?</h1>
-      <p className="p1 deletemodal__text">
-        Please confirm that you’d like to delete the {name} from the list of
-        warehouses. You won’t be able to undo this action.
-      </p>
-      <button className="secondary-button deletemodal__cancel"
-        onClick={() => {
-          onCancel(); // 关闭模态
-        }}
-      >
-        Cancel
-      </button>
-      <button className="delete-button deletemodal__delete"
-        onClick={() => {
-          onConfirm(); // 处理确认动作，比如导航
-          onCancel();
-        }}
-      >
-        Delete
-      </button>
+    <div className="deletemodal__backdrop">
+      <div className="deletemodal">
+        <img
+          src={close}
+          alt="close"
+          className="deletemodal__close"
+          onClick={onCancel}
+        />
+        <h1>Delete {name} warehouse?</h1>
+        <p className="p1 deletemodal__text">
+          Please confirm that you’d like to delete the {name} from the list of
+          warehouses. You won’t be able to undo this action.
+        </p>
+        <button
+          className="secondary-button deletemodal__cancel"
+          onClick={() => {
+            onCancel(); //close modal
+          }}
+        >
+          Cancel
+        </button>
+        <button
+          className="delete-button deletemodal__delete"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
