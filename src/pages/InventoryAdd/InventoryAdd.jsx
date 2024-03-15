96377e598c861f7c4c0aaecc297b1e8b97f9f3ba
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-15 08:32:43 -0600

    adds header

--- src/pages/InventoryAdd/InventoryAdd.jsx
+++ src/pages/InventoryAdd/InventoryAdd.jsx
@@ -1,8 +1,24 @@
-function InventoryAdd(){
-    return (
-        <>
-            <p>Hello InventoryAdd!</p>
-        </>
-    )
+import "./InventoryAdd.scss";
+import React from "react";
+import backArrow from "../../assets/icons/arrow_back-24px.svg"
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function InventoryAdd() {
+
+
+
+
+  return (
+    <main className="add-item">
+      <section className="add-item__title-wrap">
+        <img className="add-item__chevron" src={backArrow} alt="back arrow" />
+        <h1 className="add-item__title">Add New Inventory Item</h1>
+      </section>
+      <hr />
+      <section className="warehouses-list-section"></section>
+    </main>
+  );
 }
 export default InventoryAdd;
