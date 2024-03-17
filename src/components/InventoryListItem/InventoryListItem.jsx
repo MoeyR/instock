44f68b8b9c6b0dff36603d68318c6ab8dd8bb2d1
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-17 00:43:04 -0600

    adds animations to arrow

--- src/components/InventoryListItem/InventoryListItem.jsx
+++ src/components/InventoryListItem/InventoryListItem.jsx
@@ -21,55 +21,59 @@ function InventoryListItem({ inventoryItem }) {
 
   return (
     <>
-    <li className="inventory-items">
-      <div className="tablet-wrap">
-        <section className="inventory-items__wrap">
-          <div className="name-category">
-            <div className="inventory-items__item-wrap item-link-wrap">
-              <h4 className="inventory-items__heading">INVENTORY ITEM</h4>
-              <Link className="link-style inventory-items__detail" to={`/inventory/${inventoryItem.id}`}>
-                <p>{inventoryItem.item_name}</p>
-                <span className="right-arrow"> </span>
-              </Link>
+      <li className="inventory-items">
+        <div className="tablet-wrap">
+          <section className="inventory-items__wrap">
+            <div className="name-category">
+              <div className="inventory-items__item-wrap item-link-wrap">
+                  <h4 className="inventory-items__heading">INVENTORY ITEM</h4>
+                  <Link
+                    className="link-style inventory-items__detail"
+                    to={`/inventory/${inventoryItem.id}`}
+                  >
+                    <p>{inventoryItem.item_name}</p>
+                    <span className="right-arrow"> </span>
+                  </Link>
+                </div>
+              
+              <div className="inventory-items__item-wrap">
+                <h4 className="inventory-items__heading">CATEGORY</h4>
+                <p className="inventory-items__detail">
+                  {inventoryItem.category}
+                </p>
+              </div>
             </div>
-            <div className="inventory-items__item-wrap">
-              <h4 className="inventory-items__heading">CATEGORY</h4>
-              <p className="inventory-items__detail">
-                {inventoryItem.category}
-              </p>
-            </div>
-          </div>
-          <div className="status-qty-warehouse">
-            <div className="inventory-items__item-wrap status-wrap">
-              <h4 className="inventory-items__heading">STATUS</h4>
-              <p
-                className={`inventory-items__detail ${
-                  inventoryItem.status === "In Stock"
-                    ? "in-stock"
-                    : "out-of-stock"
-                }`}
-              >
-                {inventoryItem.status}
-              </p>
-            </div>
+            <div className="status-qty-warehouse">
+              <div className="inventory-items__item-wrap status-wrap">
+                <h4 className="inventory-items__heading">STATUS</h4>
+                <p
+                  className={`inventory-items__detail ${
+                    inventoryItem.status === "In Stock"
+                      ? "in-stock"
+                      : "out-of-stock"
+                  }`}
+                >
+                  {inventoryItem.status}
+                </p>
+              </div>
 
-            <div className="inventory-items__item-wrap">
-              <h4 className="inventory-items__heading">QTY</h4>
-              <p className="inventory-items__detail">
-                {inventoryItem.quantity}
-              </p>
-            </div>
+              <div className="inventory-items__item-wrap">
+                <h4 className="inventory-items__heading">QTY</h4>
+                <p className="inventory-items__detail">
+                  {inventoryItem.quantity}
+                </p>
+              </div>
 
-            <div className="inventory-items__item-wrap">
-              <h4 className="inventory-items__heading">WAREHOUSE</h4>
-              <p className="inventory-items__detail">
-                {inventoryItem.warehouse_name}
-              </p>
+              <div className="inventory-items__item-wrap">
+                <h4 className="inventory-items__heading">WAREHOUSE</h4>
+                <p className="inventory-items__detail">
+                  {inventoryItem.warehouse_name}
+                </p>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        {/* icons */}
+          {/* icons */}
           <div className="inventory-items__icons-wrap">
             <img
               className="delete-icon"
@@ -77,22 +81,18 @@ function InventoryListItem({ inventoryItem }) {
               alt="delete icon"
               onClick={openModal}
             />
-            <img
-              className="edit-icon"
-              src={editIcon}
-              alt="edit icon"
-            />
+            <img className="edit-icon" src={editIcon} alt="edit icon" />
           </div>
-      </div>
-    </li>
-          <DeleteModal
-          name={inventoryItem.item_name}
-          id={inventoryItem.id}
-          isOpen={isModalOpen}
-          onCancel={closeModal}
-          onConfirm={handleConfirm}
-        />
-        </>
+        </div>
+      </li>
+      <DeleteModal
+        name={inventoryItem.item_name}
+        id={inventoryItem.id}
+        isOpen={isModalOpen}
+        onCancel={closeModal}
+        onConfirm={handleConfirm}
+      />
+    </>
   );
 }
 
